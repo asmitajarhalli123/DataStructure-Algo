5f3eb57b5fecd904e024a21630a2da71fe2f8f65
Asmita Jarhalli <asmitajarhalli123@gmail.com>
2026-07-09 00:04:40 +0530

    updated deletion program

--- deletion.java
+++ deletion.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 public class deletion {
     public static void main(String[] args) {
           Scanner sc = new Scanner(System.in);
-         int a[]= {10 , 20 , 30 , 40 , 50 , 60};
+
+        int a[]= {10 , 20 , 30 , 40 , 50 , 60};
         int size = 5;
 
         System.out.println("Array Elements before insertion");
@@ -15,17 +16,17 @@ public class deletion {
        System.out.println("Enter position for deleting Element index (0-5):");
        int pos = sc.nextInt();
 
-        System.out.println("Enter Element :");
-        int elem = sc.nextInt();
-
-        // shift elem to right
-        for(int i = size ; i >= pos ; i--){
-            a[i] = a[i-1];
+       if(pos<= 0 || pos > size){
+            System.out.println("invalid position");
+       }
+        else{
+            for(int i=pos-1 ; i<size ; i++){
+                a[i] = a[i+1];
+            }
+            size--;
         }
 
-        // insert new elem
-        a[pos-1] = elem;
-        size++;
+        
 
         System.out.println("Array Elements after insertion");
         for(int i = 0 ; i < size ; i++){
